9a93793cde5b88a26d517a88ce79a1ad73aaaa7d
Israel Arcos <israel.arcos2595@gmail.com>
2024-02-07 17:30:54 -0500

    feat: add my app

---
+++ amplify/auth/resource.ts
@@ -0,0 +1,12 @@
+import { defineAuth } from '@aws-amplify/backend';
+
+/**
+ * Define and configure your auth resource
+ * When used alongside data, it is automatically configured as an auth provider for data
+ * @see https://docs.amplify.aws/gen2/build-a-backend/auth
+ */
+export const auth = defineAuth({
+  loginWith: {
+    email: true,
+  },
+});
